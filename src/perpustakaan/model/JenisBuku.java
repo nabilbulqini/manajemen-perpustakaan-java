06fce0a38b636b97f87bd7130147f72055a09c03
nabilbulqini <nabilbulqini@gmail.com>
2026-01-21 19:24:57 +0700

    Initial project structure

---
+++ src/perpustakaan/model/JenisBuku.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perpustakaan.model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public enum JenisBuku {
+    Novel,
+    Pelajaran,
+    Majalah
+}
---
+++ src/perpustakaan/model/StatusBuku.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perpustakaan.model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public enum StatusBuku {
+    TERSEDIA,
+    DIPINJAM
+}
